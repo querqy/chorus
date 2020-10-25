6d9ae337b648e8d379484f956c3a755d1302c3ba
epugh <epugh@opensourceconnections.com>
2020-10-25 13:54:23 -0400

    one more log

--- solr/wait-for-zk-200.sh
+++ solr/wait-for-zk-200.sh
@@ -1,1 +1,2 @@
+echo "Polling for security.json existence"
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8983/solr/admin/zookeeper?path=/security.json&detail=true)" != "200" ]]; do sleep 5; done'
