bb57909c2fd38a5dfd93e08d4a07d03e85268a05
epugh@opensourceconnections.com <>
2020-12-02 17:15:36 -0500

    CI tests earning their corn

--- quickstart/setup-observability.sh
+++ quickstart/setup-observability.sh
@@ -5,6 +5,6 @@ MINOR='\033[0;37m[QUICKSTART]    '
 RESET='\033[0m' # No Color
 
 echo -e "${MAJOR}Setting up Grafana${RESET}"
-curl -u admin:password -S -X POST -H "Content-Type: application/json" -d '{"email":"admin@choruselectronics.com", "name":"Chorus Admin", "role":"admin", "login":"admin@choruselectronics.com", "password":"password", "theme":"light"}' http://grafana:9091/api/admin/users
-curl -u admin:password -S -X PUT -H "Content-Type: application/json" -d '{"isGrafanaAdmin": true}' http://grafana:9091/api/admin/users/2/permissions
-curl -u admin:password -S -X POST -H "Content-Type: application/json" http://grafana:9091/api/users/2/using/1
+curl -u admin:password -S -X POST -H "Content-Type: application/json" -d '{"email":"admin@choruselectronics.com", "name":"Chorus Admin", "role":"admin", "login":"admin@choruselectronics.com", "password":"password", "theme":"light"}' http://grafana:3000/api/admin/users
+curl -u admin:password -S -X PUT -H "Content-Type: application/json" -d '{"isGrafanaAdmin": true}' http://grafana:3000/api/admin/users/2/permissions
+curl -u admin:password -S -X POST -H "Content-Type: application/json" http://grafana:3000/api/users/2/using/1
