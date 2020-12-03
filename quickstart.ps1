8be8c31ae8400c7f2c85a40fce6a6e69113b50fd
epugh@opensourceconnections.com <>
2020-12-03 10:06:32 -0500

    minimal quickstart

--- quickstart.ps1
+++ quickstart.ps1
@@ -1,1 +1,16 @@
-docker-compose -v
+# This script starts up Chorus and runs through the basic setup tasks.
+
+docker-compose down -v
+docker-compose up -d --build quickstart blacklight solr1 solr2 solr3 smui quepid rre grafana solr-exporter
+
+docker-compose run quickstart /quickstart/setup-solr-cluster.sh
+
+docker-compose run quickstart /quickstart/setup-smui.sh
+
+docker-compose run --rm quepid bin/rake db:setup
+docker-compose run quepid thor user:create -a admin@choruselectronics.com "Chorus Admin" password
+
+docker-compose run rre mvn rre:evaluate
+docker-compose run rre mvn rre-report:report
+
+docker-compose run quickstart /quickstart/setup-observability.sh
