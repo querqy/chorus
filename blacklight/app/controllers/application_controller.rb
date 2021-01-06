5ce346460bfbf9c2c8456a9b43d83fd387a50a24
epugh@opensourceconnections.com <>
2021-01-06 08:05:53 -0500

    experimenting with overriding default layout

--- blacklight/app/controllers/application_controller.rb
+++ blacklight/app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   include Blacklight::Controller
   layout :determine_layout if respond_to? :layout
 
+  # this method appears to change the layout.
+  def determine_layout
+    'chorus'
+  end
+
+  # this method is what the docs SAY you should change to change the layout
+  def layout_name
+    'chorus'
+  end
+
 end
