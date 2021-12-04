4dac0179fd5c574b60724801942d69603f4af963
epugh@opensourceconnections.com <>
2021-12-03 20:57:47 -0500

    now pushing normal and prelive versions

--- smui/conf/smui2solrcloud.sh
+++ smui/conf/smui2solrcloud.sh
@@ -11,7 +11,7 @@ TARGET_SYSTEM=$6
 REPLACE_RULES_SRC_TMP_FILE=$7
 REPLACE_RULES_DST_CP_FILE_TO=$8
 
-echo "In smui2solrcloud.sh - script deploying rules to querqy api"
+echo "In smui2solrcloud.sh - script deploying rules to Solr for Querqy"
 echo "^-- SRC_TMP_FILE = $SRC_TMP_FILE"
 echo "^-- DST_CP_FILE_TO = $DST_CP_FILE_TO"
 echo "^-- SOLR_HOST = $SOLR_HOST"
@@ -26,16 +26,34 @@ echo "^-- REPLACE_RULES_DST_CP_FILE_TO = $REPLACE_RULES_DST_CP_FILE_TO"
 
 echo "^-- Perform rules.txt deployment (decompound-rules.txt eventually)"
 
+function common_rules_rewriter_url() {
+  if [ $TARGET_SYSTEM == "LIVE" ]; then
+    URL="http://$SOLR_HOST/solr/$SOLR_COLLECTION_NAME/querqy/rewriter/common_rules"
+  else
+    URL="http://$SOLR_HOST/solr/$SOLR_COLLECTION_NAME/querqy/rewriter/common_rules_prelive"
+  fi
+}
+function replace_rewriter_url() {
+  if [ $TARGET_SYSTEM == "LIVE" ]; then
+    URL="http://$SOLR_HOST/solr/$SOLR_COLLECTION_NAME/querqy/rewriter/replace"
+  else
+    URL="http://$SOLR_HOST/solr/$SOLR_COLLECTION_NAME/querqy/rewriter/replace_prelive"
+  fi
+}
+
 echo "^-- ... rules.txt"
-python3 /smui/conf/push_common_rules.py $SRC_TMP_FILE "http://$SOLR_HOST/solr/$SOLR_COLLECTION_NAME/querqy/rewriter/common_rules"
+common_rules_rewriter_url $1
+
+python3 /smui/conf/push_common_rules.py $SRC_TMP_FILE $URL
 if [ $? -ne 0 ]; then
   exit 16
 fi
 
 echo "^-- ... replace-rules.txt"
+replace_rewriter_url $1
 if ! [[ $REPLACE_RULES_SRC_TMP_FILE == "NONE" && $REPLACE_RULES_DST_CP_FILE_TO == "NONE" ]]
 then
-    python3 /smui/conf/push_replace.py $REPLACE_RULES_SRC_TMP_FILE "http://$SOLR_HOST/solr/$SOLR_COLLECTION_NAME/querqy/rewriter/replace"
+    python3 /smui/conf/push_replace.py $REPLACE_RULES_SRC_TMP_FILE $URL
 fi
 
 # all ok
