986fef274b481062fa6f0b7a41734f69317df6a0
Kevin Risden <krisden@opensourceconnections.com>
2022-01-24 22:35:48 +0000

    Use osc.risden.network instead of localhost/keycloak

--- blacklight/config/environments/development.rb
+++ blacklight/config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.hosts << "chorus.dev.o19s.com"
+  config.hosts << "osc.risden.network"
 
   # Do not eager load code on boot.
   config.eager_load = false
