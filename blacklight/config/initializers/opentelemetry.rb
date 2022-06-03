ed17a89f63712306089fe45a57b7403395a00f55
epugh@opensourceconnections.com <epugh@opensourceconnections.com>
2022-06-02 21:13:43 -0400

    Lets use only the telemetry we need.

--- blacklight/config/initializers/opentelemetry.rb
+++ blacklight/config/initializers/opentelemetry.rb
@@ -7,5 +7,20 @@ ENV['OTEL_TRACES_EXPORTER'] = 'jaeger'
 
 OpenTelemetry::SDK.configure do |c|
   c.service_name = 'blacklight'
-  c.use_all() # enables all instrumentation!  We should prune this down.
+  #c.use_all() # enables all instrumentation!  We should prune this down.
+  ##### Instruments
+  c.use 'OpenTelemetry::Instrumentation::Rack'
+  #c.use 'OpenTelemetry::Instrumentation::ActionPack'
+  #c.use 'OpenTelemetry::Instrumentation::ActionView'
+  #c.use 'OpenTelemetry::Instrumentation::ActiveJob'
+  c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
+  c.use 'OpenTelemetry::Instrumentation::ConcurrentRuby'
+  #c.use 'OpenTelemetry::Instrumentation::Faraday'
+  #c.use 'OpenTelemetry::Instrumentation::HttpClient'
+  c.use 'OpenTelemetry::Instrumentation::Net::HTTP'
+  c.use 'OpenTelemetry::Instrumentation::Rails'
+  #c.use 'OpenTelemetry::Instrumentation::Redis'
+  #c.use 'OpenTelemetry::Instrumentation::RestClient'
+  #c.use 'OpenTelemetry::Instrumentation::RubyKafka'
+  #c.use 'OpenTelemetry::Instrumentation::Sidekiq'
 end
