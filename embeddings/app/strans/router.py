51ebc6d994524b5789f8f17ca96116dc5ed56fd9
René Kriegler <post@rene-kriegler.com>
2023-02-02 23:00:19 +0100

    First cut on embeddings service #136

---
+++ embeddings/app/strans/router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from strans.model import get_text_sentence_embedding
+
+from embeddings import EmbeddingsTextRequest, OutputFormat
+
+router = APIRouter()
+
+
+@router.post("/text/")
+def get_text_embedding(req_text: EmbeddingsTextRequest):
+    emb = get_text_sentence_embedding(req_text.text, req_text.normalize)
+    val = req_text.separator.join(map(str,emb)) if req_text.output_format == OutputFormat.STRING else emb.tolist()
+    return {"embedding": val}
