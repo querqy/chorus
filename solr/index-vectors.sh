104f3996e0dff9fb28aae9cdc5407880c1b76ccb
Eric Pugh <epugh@opensourceconnections.com>
2023-02-08 11:27:56 -0500

    bit o cleanup, store files in the solr/data/ dir

---
+++ solr/index-vectors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DATA_DIR="./solr/data"
+
+if [ ! -f ./data/products-vectors-1.json ]; then
+	echo -e "${MAJOR}Downloading the products-vectors-1.json.${RESET}"
+	curl --progress-bar -o ./solr/data/products-vectors-1.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-02-08/products-vectors-1.json
+fi
+if [ ! -f ./data/products-vectors-2.json ]; then
+	echo -e "${MAJOR}Downloading the products-vectors-2.json.${RESET}"
+	curl --progress-bar -o ./solr/data/products-vectors-2.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-02-08/products-vectors-2.json
+fi
+if [ ! -f ./data/products-vectors-1.json ]; then
+	echo -e "${MAJOR}Downloading the products-vectors-3.json.${RESET}"
+	curl --progress-bar -o ./solr/data/products-vectors-3.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-02-08/products-vectors-3.json
+fi
+if [ ! -f ./data/products-vectors-1.json ]; then
+	echo -e "${MAJOR}Downloading the products-vectors-4.json.${RESET}"
+	curl --progress-bar -o ./solr/data/products-vectors-4.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-02-08/products-vectors-4.json
+fi
+
+if [ -d "$DATA_DIR" ]
+then
+	if [ "$(ls -A $DATA_DIR)" ]; then
+	  cd $DATA_DIR
+	  for f in *.json;
+	    do
+	      echo "Populating products from $f , please give it a few minutes!"
+        curl --user solr:SolrRocks 'http://localhost:8983/solr/ecommerce/update?commit=true' --data-binary @"$f" -H 'Content-type:application/json ';
+       done;
+	else
+    echo "$DATA_DIR is Empty"
+	fi
+else
+	echo "Directory $DATA_DIR not found."
+fi
