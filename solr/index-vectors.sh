4e3a8d75fe4fccd645b5636ef421bbc89180eadd
Eric Pugh <epugh@opensourceconnections.com>
2023-02-17 11:00:41 -0500

    need a bit of sleep between these curls, or Eric looses a Solr node

--- solr/index-vectors.sh
+++ solr/index-vectors.sh
@@ -24,4 +24,5 @@ for f in products-vectors*.json;
   do
     echo "Populating products from ${f}, please give it a few minutes!"
     curl --user solr:SolrRocks 'http://localhost:8983/solr/ecommerce/update?commit=true' --data-binary @"$f" -H 'Content-type:application/json ';
+		sleep 5
    done;
