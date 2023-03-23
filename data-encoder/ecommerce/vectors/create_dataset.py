8cb976fa10e9041711f9295406bf513d69b1dc61
atita arora <atitaarora@gmail.com>
2023-03-23 20:41:16 +0100

    Changed AttributeError to a meaningful error message as David suggests.

--- data-encoder/ecommerce/vectors/create_dataset.py
+++ data-encoder/ecommerce/vectors/create_dataset.py
@@ -4,6 +4,7 @@ from sentence_transformers import SentenceTransformer, util
 import products
 
 model_txt = SentenceTransformer('all-MiniLM-L6-v2')
+err_msg = "Image not resolved for id : "
 
 #### Load the original products dataset
 products_dataset = products.load_products_dataset()
@@ -21,8 +22,7 @@ for idx in range(len(products_dataset)):
         products_dataset[idx]["product_vector"] = products_vectors[idx].tolist()
         products_dataset[idx]["product_image_vector"] = products_image_vectors[idx].tolist()
     except Exception:
-        print('AttributeError')
-        print(products_image_vectors[idx])
+        print(f'{err_msg}{products_dataset[idx]["id"]}')
 
 
 #### Export the new products dataset for all formats
