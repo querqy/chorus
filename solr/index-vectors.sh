689e2595ba478d79c2d1cdf6c859918cab380329
atita arora <atitaarora@gmail.com>
2023-03-24 11:36:31 +0100

    Updated the path to new vector files and added reasoning to removing model.save for clip #151

--- solr/index-vectors.sh
+++ solr/index-vectors.sh
@@ -4,19 +4,19 @@ DATA_DIR="./solr/data"
 
 if [ ! -f ./solr/data/products-vectors-1.json ]; then
   echo -e "${MAJOR}Downloading the products-vectors-1.json.${RESET}"
-  curl --progress-bar -o ./solr/data/products-vectors-1.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-02-08/products-vectors-1.json
+  curl --progress-bar -o ./solr/data/products-vectors-1.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-03-23/products-vectors-1.json
 fi
 if [ ! -f ./solr/data/products-vectors-2.json ]; then
   echo -e "${MAJOR}Downloading the products-vectors-2.json.${RESET}"
-  curl --progress-bar -o ./solr/data/products-vectors-2.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-02-08/products-vectors-2.json
+  curl --progress-bar -o ./solr/data/products-vectors-2.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-03-23/products-vectors-2.json
 fi
 if [ ! -f ./solr/data/products-vectors-3.json ]; then
   echo -e "${MAJOR}Downloading the products-vectors-3.json.${RESET}"
-  curl --progress-bar -o ./solr/data/products-vectors-3.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-02-08/products-vectors-3.json
+  curl --progress-bar -o ./solr/data/products-vectors-3.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-03-23/products-vectors-3.json
 fi
 if [ ! -f ./solr/data/products-vectors-4.json ]; then
   echo -e "${MAJOR}Downloading the products-vectors-4.json.${RESET}"
-  curl --progress-bar -o ./solr/data/products-vectors-4.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-02-08/products-vectors-4.json
+  curl --progress-bar -o ./solr/data/products-vectors-4.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-03-23/products-vectors-4.json
 fi
 
 cd $DATA_DIR
