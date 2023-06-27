c7dd749e85f7d3ea7668c5521ad5c2c0b636b383
atita arora <atitaarora@gmail.com>
2023-06-27 15:40:24 +0200

    PR Review feedback - Updated the TODO to downaload and index finetuned product vector files. Added change to remove 'mm' from config to fix hybrid search usecase.

--- solr/index-vectors.sh
+++ solr/index-vectors.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 DATA_DIR="./solr/data"
-##TODO Replace with finetuned vector file json
-: 'Replace with finetuned vector file json
-if [ ! -f ./solr/data/products-vectors-1.json ]; then
+if [ ! -f ./solr/data/finetuned-products-vectors-1.json ]; then
   echo -e "${MAJOR}Downloading the products-vectors-1.json.${RESET}"
-  curl --progress-bar -o ./solr/data/products-vectors-1.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-03-23/products-vectors-1.json
+  curl --progress-bar -o ./solr/data/finetuned-products-vectors-1.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-finetuned-vectors-2023-06/finetuned-products-vectors-1.json
 fi
-if [ ! -f ./solr/data/products-vectors-2.json ]; then
+if [ ! -f ./solr/data/finetuned-products-vectors-2.json ]; then
   echo -e "${MAJOR}Downloading the products-vectors-2.json.${RESET}"
-  curl --progress-bar -o ./solr/data/products-vectors-2.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-03-23/products-vectors-2.json
+  curl --progress-bar -o ./solr/data/finetuned-products-vectors-2.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-finetuned-vectors-2023-06/finetuned-products-vectors-2.json
 fi
-if [ ! -f ./solr/data/products-vectors-3.json ]; then
+if [ ! -f ./solr/data/finetuned-products-vectors-3.json ]; then
   echo -e "${MAJOR}Downloading the products-vectors-3.json.${RESET}"
-  curl --progress-bar -o ./solr/data/products-vectors-3.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-03-23/products-vectors-3.json
+  curl --progress-bar -o ./solr/data/finetuned-products-vectors-3.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-finetuned-vectors-2023-06/finetuned-products-vectors-3.json
 fi
-if [ ! -f ./solr/data/products-vectors-4.json ]; then
+if [ ! -f ./solr/data/finetuned-products-vectors-4.json ]; then
   echo -e "${MAJOR}Downloading the products-vectors-4.json.${RESET}"
-  curl --progress-bar -o ./solr/data/products-vectors-4.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-vectors-2023-03-23/products-vectors-4.json
+  curl --progress-bar -o ./solr/data/finetuned-products-vectors-4.json -k https://o19s-public-datasets.s3.amazonaws.com/chorus/product-finetuned-vectors-2023-06/finetuned-products-vectors-4.json
 fi
-'
 
 cd $DATA_DIR
 for f in finetuned-products-vectors*.json;
