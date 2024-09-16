60b45e02017fe7e87659584d4b6999c15faea9b9
Eric Pugh <epugh@opensourceconnections.com>
2024-09-16 14:04:25 -0400

    nicer help message

--- helpers.sh
+++ helpers.sh
@@ -81,10 +81,10 @@ function check_prerequisites() {
 }
 
 function display_help_message() {
-    echo -e "Use the option --with-offline-lab | -lab to include Quepid and RRE services in Chorus."
+    echo -e "Use the option --with-offline-lab | -lab to include Quepid in Chorus."
     echo -e "Use the option --with-observability | -obs to include Grafana, Prometheus, and Solr Exporter services in Chorus."
+    echo -e "Use the option --with-active-search-management | -active to include Active Search Management via SMUI in Chorus."    
     echo -e "Use the option --with-vector-search | -vector to include Vector Search services in Chorus."
-    echo -e "Use the option --with-active-search-management | -active to include Active Search Management in Chorus."
     echo -e "Use the option --shutdown | -s to shutdown and remove the Docker containers and data."
     echo -e "Use the option --online-deployment | -online to update configuration to run on chorus.dev.o19s.com environment."
 }
